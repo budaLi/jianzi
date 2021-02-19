01f77a8b9a7914140fd53d455452614428f883ec
lenovo <1364826576@qq.com>
2021-02-19 16:54:39 +0800

    链表逆序

---
+++ leetcode/单向链表逆序输出.py
@@ -0,0 +1,50 @@
+# coding=utf-8
+# Copyright (c) 2020 ichinae.com, Inc. All Rights Reserved
+"""
+Module Summary Here.
+Authors: lijinjun1351@ichinae.com
+"""
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        current = head.next
+        head.next = None
+        while current:
+            x = current.next
+            current.next = head
+            head = current
+            current = x
+
+        return head
+
+S = Solution()
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+head = node1
+
+res = S.reverseList(head)
+while res:
+    print(res.val)
+    res = res.next
+
+
