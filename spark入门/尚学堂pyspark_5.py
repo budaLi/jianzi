f53d0e50da8e4eabbeaf83b6f39989d29c5fe30a
lenovo <1364826576@qq.com>
2021-03-22 17:29:59 +0800

    修复bug

--- spark入门/尚学堂pyspark_5.py
+++ spark入门/尚学堂pyspark_5.py
@@ -7,7 +7,7 @@ Authors: lijinjun1351@ichinae.com
 import random
 from pyspark.context import  SparkContext
 from pyspark import SparkConf
-
+import py4j
 def mock_data(number):
     """
     mock 数据
@@ -48,7 +48,7 @@ def mock_data(number):
             random.choice(action),
         )
         totle_data.append(data)
-
+    totle_data = list(set(totle_data))
     with open("data.txt","w") as f:
         for one in totle_data:
             print(one)
@@ -56,22 +56,24 @@ def mock_data(number):
 
 def get_top3_uid_and_cnt(info):
     """ info 为某site 对应的用户访问个数 """
+    site = info[0]
+    uid_cnt = info[1]
 
-    # site = info[0]
-    # uid_cnt = info[1]
-    #
-    # top_res = ["","",""]
-    # for one in uid_cnt:
-    #     uid = one[0]
-    #     cnt = one[1]
-    #     if top_res[2]=="":
-    #         top_res[2] = cnt
-    #     elif cnt>top_res[2]:
-    #         top_res[2] = cnt
-    #         top_res[0] = site
-    #         top_res[1] = uid
-    # top_res = tuple(top_res)
-    return {"1":123}
+    top_res = ["","",""]
+    for one in uid_cnt:
+        uid = one[0]
+        cnt = one[1]
+        for i in range(len(top_res)):
+            if top_res[i]=="":
+                top_res[i] = one
+                break
+            elif top_res[i][1]<cnt:
+                for j in range(2,i,-1):
+                    top_res[j] = top_res[j-1]
+                top_res[i] = one
+                break
+
+    return site,top_res
 
 # 统计每个网站每个用户访问的次数
 def get_site_uid_cnt(info):
@@ -88,7 +90,8 @@ def get_site_uid_cnt(info):
     # 统计当前site 每个用户出现的次数
     res = []
     for uid, cnt in info_cnt.items():
-        tem = (site, uid, cnt)
+        # TODO 之后的groupbykey需要设置成如下形式
+        tem = (site,(uid, cnt))
         res.append(tem)
     # res = tuple(res)
     return res
@@ -106,7 +109,7 @@ def get_top_3():
         groupByKey()\
         .flatMap(lambda info:get_site_uid_cnt(info))\
         .groupByKey()\
-        .map(lambda x:get_top3_uid_and_cnt(x))\
+        .map(lambda info:get_top3_uid_and_cnt(info))\
         .foreach(print)
 
 
